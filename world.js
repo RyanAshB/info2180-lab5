d9de145db2ea391509664c5db6bf45ae52ef0413
RyanAshB <ryanash.b@gmail.com>
2023-11-20 21:00:02 -0500

    Exercise 4 Complete

--- world.js
+++ world.js
@@ -1,24 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Attach a click event listener to the 'Lookup' button
     document.getElementById("lookup").addEventListener("click", function () {
-        // Get the country name entered by the user
         var countryName = document.getElementById("country").value;
 
-        // Perform an AJAX request to world.php with the country parameter
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
-                    // Directly append the HTML to the result div
                     document.getElementById("result").innerHTML = xhr.responseText;
                 } else {
-                    // Handle the error
                     console.error("Error fetching data: ", xhr.statusText);
                 }
             }
         };
 
-        // Construct the URL with the country parameter
         var url = "world.php?country=" + encodeURIComponent(countryName);
         xhr.open("GET", url, true);
         xhr.send();
--- world.php
+++ world.php
@@ -4,36 +4,31 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-// Establish a connection to the database
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-// Check if the 'country' parameter is set in the GET request
 if (isset($_GET['country'])) {
     $country = $_GET['country'];
 
-    // Use a prepared statement to avoid SQL injection
     $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
     $stmt->bindParam(':country', $country, PDO::PARAM_STR);
     $stmt->execute();
 
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Output the HTML table directly
     if (!empty($results)) {
         echo '<table>
                 <thead>
                     <tr>';
         
-        // Assuming the first row in the result contains all necessary keys
         foreach ($results[0] as $key => $value) {
-            echo '<th>' . ucfirst(str_replace('_', ' ', $key)) . '</th>'; // Capitalize and replace underscores with spaces
+            echo '<th>' . ucfirst(str_replace('_', ' ', $key)) . '</th>'; 
         }
         
         echo '</tr>
                 </thead>
                 <tbody>';
         
-        // Iterate through the results and create rows
         foreach ($results as $row) {
             echo '<tr>';
             foreach ($row as $value) {
@@ -48,7 +43,6 @@ if (isset($_GET['country'])) {
         echo '<p>No results found for the provided country.</p>';
     }
 } else {
-    // Handle the case where 'country' parameter is not set
     echo '<p>Please provide a country name.</p>';
 }
 ?>
